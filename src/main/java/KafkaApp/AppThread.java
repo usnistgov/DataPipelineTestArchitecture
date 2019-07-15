7884c221b71302cd8c64bf7405fb3dd8207933f7
djharten <djharten@millersville.edu>
2019-07-15 10:22:29 -0400

    new update

---
+++ src/main/java/KafkaApp/AppThread.java
@@ -0,0 +1,59 @@
+package Kafka;
+
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.CountDownLatch;
+
+public class AppThread {
+
+    public static void main(String[] args) {
+        new AppThread().run();
+    }
+
+    private AppThread() {
+
+    }
+
+    private void run() {
+        Logger logger = LoggerFactory.getLogger(AppThread.class.getName());
+        KafkaConnector connector = new KafkaConnector();
+        connector.consume();
+        KafkaConsumer<String, String> consumer = connector.getConsumer();
+        KafkaProducer<String, String> producer = connector.getProducer();
+
+        /*
+         * Latch is used to deal with multiple threads.
+         */
+        CountDownLatch latch = new CountDownLatch(1);
+
+        logger.info("Creating consumer and producer threads.");
+
+        Runnable runnable = new KafkaRunnable(consumer, producer, latch, connector.getTopicProducer());
+        Thread runnableThread = new Thread(runnable);
+        runnableThread.start();
+
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            logger.info("Caught shutdown hook.");
+            ((KafkaRunnable) runnable).shutdown();
+            try {
+                latch.await();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            logger.info("Application has exited");
+        }
+        ));
+
+        try {
+            latch.await();
+        } catch(InterruptedException e) {
+            logger.error("Application Interrupted");
+        } finally {
+            logger.info("application is closing.");
+        }
+
+    }
+}
---
+++ src/main/java/KafkaApp/KafkaConnector.java
@@ -0,0 +1,75 @@
+package Kafka;
+
+
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+import java.util.Collections;
+import java.util.Properties;
+
+public class KafkaConnector {
+    /*
+     * Set your server, groupId and topic information accordingly.
+     */
+    private final Logger logger = LoggerFactory.getLogger(Consumer.class.getName());
+    private Properties consumerProperties;
+    private Properties producerProperties;
+    private KafkaConsumer<String, String> consumer;
+    private KafkaProducer<String, String> producer;
+    private final String bootstrapServer ="127.0.0.1:29092";
+    private final String groupIdConsumer = "my-first-application";
+    private final String topicConsumer = "connect-custom";
+    private final String groupIdProducer = "json-group";
+    private final String topicProducer = "json_topic";
+
+    KafkaConnector() {
+        this.consumerProperties = new Properties();
+        this.consumerProperties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServer);
+        this.consumerProperties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        this.consumerProperties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        this.consumerProperties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupIdConsumer);
+        this.consumerProperties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+        this.consumer = new KafkaConsumer<String, String>(consumerProperties);
+
+        this.producerProperties = new Properties();
+        this.producerProperties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServer);
+        this.producerProperties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        this.producerProperties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        this.producer = new KafkaProducer<String, String>(producerProperties);
+    }
+
+    Consumer consume(){
+        this.consumer.subscribe(Collections.singletonList(topicConsumer));
+        return this.consumer;
+    }
+
+    /**************************************/
+    /* GETTERS AND SETTERS */
+    /**************************************/
+
+    public String getTopicProducer() {
+        return this.topicProducer;
+    }
+
+    public KafkaConsumer<String, String> getConsumer() {
+        return this.consumer;
+    }
+
+    public KafkaProducer<String, String> getProducer() {
+        return this.producer;
+    }
+
+}
