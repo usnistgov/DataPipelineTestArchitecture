f3e3c1e5b38a07bf4cefd7eb6c7fcdc8bbbb086b
Sprock <tas5@NIST.GOV>
2019-10-15 15:41:04 -0400

    add configuration properties
    moved code to functions in Source Task
    add setupTaskConfig with URL, nextSequence, and Topic properties
    started some test coverage.

--- mtconnectsourceconnector/src/main/java/gov/nist/mtconnect/MTConnectSourceTask.java
+++ mtconnectsourceconnector/src/main/java/gov/nist/mtconnect/MTConnectSourceTask.java
@@ -2,9 +2,6 @@ package gov.nist.mtconnect;
 
 import org.apache.kafka.connect.source.SourceRecord;
 import org.apache.kafka.connect.source.SourceTask;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.apache.commons.io.IOUtils;
 import org.w3c.dom.*;
 import org.xml.sax.SAXException;
 
@@ -23,19 +20,25 @@ import java.io.StringWriter;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.List;
+import java.util.ArrayList;
 import java.util.Map;
+import java.util.Collections;
 import java.net.HttpURLConnection;
 
 public class MTConnectSourceTask extends SourceTask {
   //static final Logger log = LoggerFactory.getLogger(MTConnectSourceTask.class);
 
-  private static final String DEVICE_URL = "http://mtconnect.mazakcorp.com:5609"; // should be input from config
-  private String INITIAL_URL = this.DEVICE_URL+"/current";
-  private String SEQUENCE_URL = this.DEVICE_URL+"/sample?from=";
-  private String currentURL;
-  private InputStream responseDocument;
+  private static final String DEVICE_URL = "device_url";
+  private static final String LAST_SEQUENCE = "last_sequence";
+  private static final String TOPIC_CONFIG = "topic_config";
+  private String deviceURL = "http://mtconnect.mazakcorp.com:5609"; // should be input from config
+  private String initialUrl = this.deviceURL+"/current";
+  private String sequenceUrl = this.deviceURL+"/sample?from=";
+  private String topic = deviceURL+"XML";
+  private InputStream responseStream;
   private Document xmlResponseDocument;
-  private int nextSequence = -1;
+  private Integer nextSequence = -1;
+  private Integer lastSequence = -1;
 
   @Override
   public String version() {
@@ -43,8 +46,22 @@ public class MTConnectSourceTask extends SourceTask {
   }
 
   @Override
-  public void start(Map<String, String> map) {
+  public void start(Map<String, String> props) {
     //TODO: Do things here that are required to start your task. This could be open a connection to a database, etc.
+      this.setupTaskConfig(props);
+
+      //log.debug("Trying to get persistedMap.");
+      /*
+      Map<String, Object> persistedMap = null;
+      if (context != null && context.offsetStorageReader() != null) {
+          // Lookup the offset by the device url (the key is stored as a singletonMap)
+          persistedMap = context.offsetStorageReader().offset(Collections.singletonMap(DEVICE_URL, deviceURL));
+      }
+      //log.info("The persistedMap is {}", persistedMap);
+      if (persistedMap != null) {
+          this.lastSequence = (Integer) persistedMap.get(LAST_SEQUENCE);
+      }
+      */
   }
 
   @Override
@@ -55,91 +72,116 @@ public class MTConnectSourceTask extends SourceTask {
         // if nextSequence == -1, get Current
       try {
           if (this.nextSequence == -1) {
-              url = new URL(this.INITIAL_URL);
+              url = new URL(this.initialUrl);
           } else { //else append nextSequence to http
-              url = new URL(this.SEQUENCE_URL + nextSequence);
+              url = new URL(this.sequenceUrl + nextSequence);
           }
-      }
-        catch (MalformedURLException e) {
-          e.printStackTrace();
-      }
 
-        try {
           urlConnection = (HttpURLConnection) url.openConnection();
           urlConnection.setRequestMethod("GET");
           urlConnection.setRequestProperty("Accept", "application/xml");
           int responseCode = urlConnection.getResponseCode();
           if (responseCode == HttpURLConnection.HTTP_OK){
-              this.responseDocument = urlConnection.getInputStream();
+              responseStream = urlConnection.getInputStream();
           }
-        } catch (IOException e) {
-          e.printStackTrace();
-        }
-
-      DocumentBuilderFactory domFact = DocumentBuilderFactory.newInstance();
-      DocumentBuilder builder = null;
-      DOMSource domSource = null;
-      StringWriter writer = new StringWriter();
-      StreamResult result = new StreamResult(writer);
-
-      try {
-          builder = domFact.newDocumentBuilder();
-          this.xmlResponseDocument = builder.parse(this.responseDocument);
-          domSource = new DOMSource(this.xmlResponseDocument);
-          TransformerFactory tf = TransformerFactory.newInstance();
-          Transformer transformer = tf.newTransformer();
-          transformer.transform(domSource, result);
-
-      } catch (ParserConfigurationException | TransformerException | SAXException | IOException e) {
+      }
+        catch (IOException e) {
           e.printStackTrace();
       }
 
-      Element root = this.xmlResponseDocument.getDocumentElement();
+      xmlResponseDocument = this.parseStream(responseStream);
+      Element root = xmlResponseDocument.getDocumentElement();
       String xmlResponseDocumentType = root.getNodeName();
       System.out.println("Response Document Type: " + xmlResponseDocumentType);
 
     // If Response is MTConnectStreams (responseDocument.firstNode.nodeType == "MTConnectStreams")
       if (xmlResponseDocumentType.equals("MTConnectStreams")){
           System.out.println("Getting sequence numbers");
-          int[] sequenceNumbers = getCurrentSequenceNumbers(this.xmlResponseDocument);
-          int lastSequence = sequenceNumbers[1];
-          System.out.println("Last Sequence Number: " + Integer.toString(lastSequence));
-          System.out.println("XML IN String format is: \n" + writer.toString());
+          int[] sequenceNumbers = getCurrentSequenceNumbers(xmlResponseDocument);
+          this.lastSequence = sequenceNumbers[1];
+          System.out.println("Last Sequence Number: " + this.lastSequence);
+          System.out.println("XML IN String format is: \n" + this.DOMtoStringWriter(xmlResponseDocument).toString());
+          return getReponseDocumentAsSourceRecords(xmlResponseDocument);
       }
         // Put response document into SourceRecord
         // Store the Next Sequence Number
 
     // Else If Response is MTConnect Error
-      if (xmlResponseDocumentType.equals("MTConnectError")){
+      else if (xmlResponseDocumentType.equals("MTConnectError")){
+          this.lastSequence = -1;
           System.out.println("Response Document Type: " + xmlResponseDocumentType);
-
+          return getReponseDocumentAsSourceRecords(null);
       }
-      // log error
-      // write -1 to nextSequence
     // Else (didn't get an MTConnect response at all)
-    //TODO: Create SourceRecord objects that will be sent the kafka cluster.
-      List<SourceRecord> sourceRecord = null;
-      return sourceRecord;
-   }
+      else {
+          this.lastSequence = -1;
+          System.out.println("No Response Document");
+          return Collections.emptyList();
+      }
+  }
 
   @Override
   public void stop() {
     //TODO: Do whatever is required to stop your task.
   }
 
+  private List<SourceRecord> getReponseDocumentAsSourceRecords(Document xmlResponseDocument) {
+      ArrayList<SourceRecord> records = new ArrayList<>();
+      Map<String, String> sourcePartition = Collections.singletonMap(DEVICE_URL, deviceURL);
+      Map<String, Integer> sourceOffset = Collections.singletonMap(LAST_SEQUENCE, lastSequence);
+      String value = this.DOMtoStringWriter(xmlResponseDocument).toString();
+      records.add(new SourceRecord(sourcePartition, sourceOffset, topic, null, value));
 
+      return records;
+    }
+    private void setupTaskConfig(Map<String, String> props) {
+        this.deviceURL = props.get(DEVICE_URL);
+        this.nextSequence = Integer.parseInt(props.get(LAST_SEQUENCE));
+        this.topic = props.get(TOPIC_CONFIG);
+
+    }
     private static int[] getCurrentSequenceNumbers(Document doc) {
         int[] sequenceNums = new int[2];
         NodeList nodes = doc.getElementsByTagName("Header");
         Node node = nodes.item(0);
         Element element = (Element) node;
-        //NamedNodeMap map = element.getAttributes();
-        //for(int i = 0; i < map.getLength(); i++) {
-        //    Node attr = map.item(i);
-        //    System.out.println(attr.getNodeValue() + " " + attr.getNodeName());
-        //}
+
         sequenceNums[0] = Integer.parseInt(element.getAttribute("firstSequence"));
         sequenceNums[1] = Integer.parseInt(element.getAttribute("lastSequence"));
         return sequenceNums;
     }
+
+    private Document parseStream(InputStream responseStream) {
+        DocumentBuilderFactory domFact = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder;
+
+        try {
+            builder = domFact.newDocumentBuilder();
+            xmlResponseDocument = builder.parse(responseStream);
+        } catch (ParserConfigurationException | SAXException | IOException e) {
+            e.printStackTrace();
+        }
+
+        return xmlResponseDocument;
+    }
+
+    private StringWriter DOMtoStringWriter(Document xmlResponseDocument){
+        DOMSource domSource;
+        StringWriter writer = new StringWriter();
+        StreamResult result = new StreamResult(writer);
+
+        try {
+            domSource = new DOMSource(xmlResponseDocument);
+            TransformerFactory tf = TransformerFactory.newInstance();
+            Transformer transformer = tf.newTransformer();
+            transformer.transform(domSource, result);
+        }
+        catch( TransformerException e){
+            e.printStackTrace();
+        }
+
+        return writer;
+    }
+
+
 }
--- mtconnectsourceconnector/src/test/java/gov/nist/mtconnect/MTConnectSourceTaskTest.java
+++ mtconnectsourceconnector/src/test/java/gov/nist/mtconnect/MTConnectSourceTaskTest.java
@@ -6,6 +6,8 @@ import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MTConnectSourceTaskTest {
   @Test
@@ -13,9 +15,21 @@ public class MTConnectSourceTaskTest {
       MTConnectSourceTask sourceTask = new MTConnectSourceTask();
 
       try {
+        Map<String, String> properties = new HashMap<String, String>();
+        properties.put("device_url", "http://mtconnect.mazakcorp.com:5609");
+        properties.put("last_sequence", "-1");
+        properties.put("topic_config", "5609XML");
+        sourceTask.start(properties);
+
+      }catch (){
+          System.out.println("error in start: ");
+      }
+
+
+      try {
           sourceTask.poll();
       } catch (InterruptedException e) {
           e.printStackTrace();
       }
   }
-}
+  }
