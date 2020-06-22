e44bb6c40b50a3e20db9aa0ac5fbade33ff2aa87
tim <timothy.sprock@nist.gov>
2020-06-22 13:09:55 -0500

    add another environmental variable

--- config/pipeline-env.sh
+++ config/pipeline-env.sh
@@ -5,21 +5,22 @@
 # may need to first call >sudo chmod -x ./pipeline-env.sh
 
 #Paths to major components
+export DATAPIPELINE=/home/tim/DataPipelineTestArchitecture
 export KAFKA=/home/tim/Kafka/kafka_2.12-2.5.0
 export HADOOP=/home/tim/Kafka/hadoop-3.2.1
 export SPARK=/home/tim/Kafka/spark-3.0.0-bin-hadoop3.2
 
 #Kafka
 #For add-on connectors (mtconnect, mqtt, etc.)
-export KafkaConnectors=/home/tim/DataPipelineTestArchitecture/connectors 
-export KafkaConfig=/home/tim/DataPipelineTestArchitecture/config
+export KafkaConnectors=$DATAPIPELINE/connectors 
+export KafkaConfig=$DATAPIPELINE/config
 
 #Pyspark
 export PYSPARK_PYTHON=python3
-export LD_LIBRARY_PATH=/home/tim/Kafka/hadoop-3.2.1/lib/native
+export LD_LIBRARY_PATH=$HADOOP/lib/native
 
 #MTConnect simulator
-export MTConnectAdapterSim=/etc/mtcconnect/adapter
+export MTConnectAdapterSim=/etc/mtconnect/adapter
 export MTConnectAdapterSimLog=/etc/mtconnect/adapter
 export RUBY=/usr/bin/ruby
 #export MTConnectAgent=
